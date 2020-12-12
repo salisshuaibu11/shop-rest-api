64983a93162e085b4bfbf36663a5c091711b46e0
salis <salisshuaibu11@gmail.com>
2020-12-12 12:32:57 +0000

    Created database and added Model for product

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+
+mongoose.connect(
+  "mongodb+srv://@node-rest.ltzra.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    user: process.env.MONGO_USER,
+    pass: process.env.MONGO_PASSWORD,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
